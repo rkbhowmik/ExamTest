db401d12e19b27d62738b5e64dd340d27427648c
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-09 13:38:35 +0100

    - Save personal details in a file - It will append text so you have the previous records as well

---
+++ Person Info/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Person_Info
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.txtphone = new System.Windows.Forms.TextBox();
+			this.txtemail = new System.Windows.Forms.TextBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label5 = new System.Windows.Forms.Label();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(45, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Name";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(45, 70);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "E mail";
+			this.label2.Click += new System.EventHandler(this.label2_Click_1);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(45, 98);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(55, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Phone No";
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(176, 32);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(141, 20);
+			this.txtName.TabIndex = 0;
+			// 
+			// txtphone
+			// 
+			this.txtphone.Location = new System.Drawing.Point(176, 95);
+			this.txtphone.Name = "txtphone";
+			this.txtphone.Size = new System.Drawing.Size(141, 20);
+			this.txtphone.TabIndex = 1;
+			// 
+			// txtemail
+			// 
+			this.txtemail.Location = new System.Drawing.Point(176, 63);
+			this.txtemail.Name = "txtemail";
+			this.txtemail.Size = new System.Drawing.Size(141, 20);
+			this.txtemail.TabIndex = 1;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(176, 136);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(141, 20);
+			this.dateTimePicker1.TabIndex = 3;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(45, 143);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(68, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Date Of Birth";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(176, 189);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(141, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "&Save";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(347, 230);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.txtemail);
+			this.Controls.Add(this.txtphone);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Personal Details";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.TextBox txtphone;
+		private System.Windows.Forms.TextBox txtemail;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+	}
+}
+
---
+++ Person Info/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Person_Info
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var name = txtName.Text;
+			var mail = txtemail.Text;
+			var phone = txtphone.Text.ToString();
+			var birthday = int.Parse(dateTimePicker1.Value.ToString("yyyyMMdd"));
+			var today = int.Parse(DateTime.Today.ToString("yyyyMMdd"));
+			var age = ((today - birthday) / 10000).ToString();
+			var content = $"Name: {name}, Email: {mail}, Phone No. {phone}, {age} Years old";
+
+			saveFileDialog1.ShowDialog();
+			File.AppendAllText(saveFileDialog1.FileName, content + Environment.NewLine);
+		}
+	}
+}
