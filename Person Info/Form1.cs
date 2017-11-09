03cf7fb771a1cfad00a8e0d25e519d8c24fb85d8
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-09 16:38:30 +0100

    Read File, Sorting Array

--- Person Info/Form1.cs
+++ Person Info/Form1.cs
@@ -23,15 +23,6 @@ namespace Person_Info
 			
 		}
 
-		private void label2_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void label2_Click_1(object sender, EventArgs e)
-		{
-
-		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
---
+++ ReadTextFileAndSortInArray/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadTextFileAndSortInArray
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int counter = 0;
+				string line = "";
+				StreamReader sr = new StreamReader(@"c:\users\studerande\desktop\test\sample.txt");
+				List<int> list = new List<int>();
+				while ((line = sr.ReadLine()) != null)
+				{
+					listBox1.Items.Add(line);
+					list.Add(int.Parse(line));
+					counter++;
+				}
+				int[] arr = list.ToArray();
+				Array.Sort(arr);
+				foreach (int item in arr)
+				{
+					listBox2.Items.Add(item);
+				}
+		}
+	}
+}
+
+
