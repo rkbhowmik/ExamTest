4078a3869c2d9acab9ac998aa511e128f7fcdecb
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-09 12:33:22 +0100

    -Exam Exercise..

---
+++ ReadTwoArraysAndSumInThird/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ReadTwoArraysAndSumInThird
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(74, 35);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(90, 36);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "File 1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(447, 35);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(83, 36);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "&Save";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// openFileDialog2
+			// 
+			this.openFileDialog2.FileName = "openFileDialog2";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(74, 96);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(188, 96);
+			this.richTextBox1.TabIndex = 4;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(447, 96);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(205, 96);
+			this.richTextBox2.TabIndex = 5;
+			this.richTextBox2.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(763, 261);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+	}
+}
+
---
+++ ReadTwoArraysAndSumInThird/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadTwoArraysAndSumInThird
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+			richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			saveFileDialog1.ShowDialog();
+			var content = File.ReadAllText(openFileDialog1.FileName);
+			File.WriteAllText(saveFileDialog1.FileName, content.ToUpper());
+			richTextBox2.Text = File.ReadAllText(saveFileDialog1.FileName);
+		}
+	}
+}
